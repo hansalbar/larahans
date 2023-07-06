5d5d5c8da1467603aae7b86c5a148235fc79727d
hansalbar <firdaus.hans.21@student.is.ittelkom-sby.ac.id git config --global user.name hansalbar git config --global user.email firdaus.hans.21@student.is.ittelkom-sby.ac.id>
2023-07-06 18:47:36 +0700

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,20 +9,10 @@
 </head>
 <body>
     @extends('layouts.app')
-
-@section('content')
+    @section('content')
     @include('default')
+@endsection
 
-
-    {{-- <div class="container mt-4">
-        <h4>{{ $pageTitle }}</h4>
-        <hr>
-        <div class="d-flex align-items-center py-2 px-4 bg-light rounded-3 border">
-            <div class="bi-house-fill me-3 fs-1"></div>
-            <h4 class="mb-0">Well done! this is {{ $pageTitle }}.</h4>
-        </div>
-    </div> --}}
-    @endsection
     @vite('resources/js/app.js')
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
-
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Auth\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,7 +20,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('home', [HomeController::class, 'index'])->name('home');
-Route::get('employees',  [HomeController::class, 'index'])->name('employee.index');
-Route::get('profile', ProfileController::class)->name('profile');
-Route::resource('employees', EmployeeController::class);
+
+Route::get('home', [HomeController::class, 'index'])->name('home')->middleware('auth');
+
+Route::get('employees', [HomeController::class, 'index'])->name('employees.index')->middleware('auth');
+Route::get('profile', [HomeController::class, 'index'])->name('profile')->middleware('auth');
+
+Route::get('profile', ProfileController::class)->name('profile')->middleware('auth');
+
+Route::resource('employees', EmployeeController::class)->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+
+Route::post('login', [LoginController::class, 'authenticate']);
